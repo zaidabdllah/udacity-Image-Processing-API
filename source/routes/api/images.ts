51f86d3800c757c25f590db1a71e4017373539cd
zaid abdallah <zzidxrh@gmail.com>
2026-03-05 19:58:36 +0200

    finish images route

--- source/routes/api/images.ts
+++ source/routes/api/images.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import validateImageParams from '../../middleware/images/validateImageParams';
+import getValidatedImgPath from '../../utils/images/getValidatedImgPath';
+import ProssingFilePath from '../../utils/images/ProssingFilePath';
 const images = express.Router();
 
-images.get('/', validateImageParams, (req, res) => {
-    res.status(200);
-    res.send('Image processing endpoint');
+images.get('/', validateImageParams, async (req, res): Promise<void> => {
+  const { filename, width, height } = req.query;
+  const filepath = await ProssingFilePath(filename as string, Number(width), Number(height));
+  res.status(200).sendFile(filepath as string);
 });
 
 export default images;
